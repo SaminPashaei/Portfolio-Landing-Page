930eea1998ad1e8b5ee252867548074776e45f84
SaminPashaei <saminpashaei96@gmail.com>
2023-01-18 21:20:14 +0330

    Added achievement cards html and css
    Also made responsive using javascript

---
+++ js/app.js
@@ -0,0 +1,36 @@
+function setEqualHeight() {
+  let achieveCard = document.querySelectorAll(".achieve-card");
+
+  for (let index = 0; index < achieveCard.length; index++) {
+    achieveCard[index].style.height = "fit-content";
+  }
+
+  let achieveCardHeight = [];
+  for (let index = 0; index < achieveCard.length; index++) {
+    achieveCardHeight[index] = window
+      .getComputedStyle(achieveCard[index])
+      .getPropertyValue("height");
+  }
+
+  let setHeight;
+  if (achieveCardHeight[0] > achieveCardHeight[1]) {
+    if (achieveCardHeight[0] > achieveCardHeight[2]) {
+      setHeight = achieveCardHeight[0];
+    } else {
+      setHeight = achieveCardHeight[2];
+    }
+  } else {
+    if (achieveCardHeight[1] > achieveCardHeight[2]) {
+      setHeight = achieveCardHeight[1];
+    } else {
+      setHeight = achieveCardHeight[2];
+    }
+  }
+
+  for (let index = 0; index < achieveCard.length; index++) {
+    achieveCard[index].style.height = setHeight;
+  }
+}
+
+setEqualHeight();
+window.onresize = setEqualHeight;
