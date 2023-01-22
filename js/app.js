eb57bbec4d6bd2d7abaa6a4ee9f4624311eccc32
SaminPashaei <saminpashaei96@gmail.com>
2023-01-22 17:32:56 +0330

    Finished about page

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
 function setAchieveHeight() {
-  let achieveCard = document.querySelectorAll(".achieve-card");
-
-  for (let index = 0; index < achieveCard.length; index++) {
-    achieveCard[index].style.height = "fit-content";
+  let PreAchieveCard = document.querySelectorAll(".achieve-card");
+  for (let index = 0; index < PreAchieveCard.length; index++) {
+    PreAchieveCard[index].style.height = "fit-content";
   }
 
-  let achieveCardHeight = [];
-  for (let index = 0; index < achieveCard.length; index++) {
-    achieveCardHeight[index] = window
-      .getComputedStyle(achieveCard[index])
-      .getPropertyValue("height");
-  }
+  if (window.innerWidth >= 768) {
+    let achieveCard = document.querySelectorAll(".achieve-card");
 
-  let setHeight;
-  if (achieveCardHeight[0] > achieveCardHeight[1]) {
-    if (achieveCardHeight[0] > achieveCardHeight[2]) {
-      setHeight = achieveCardHeight[0];
-    } else {
-      setHeight = achieveCardHeight[2];
+    let achieveCardHeight = [];
+    for (let index = 0; index < achieveCard.length; index++) {
+      achieveCardHeight[index] = achieveCard[index].clientHeight;
     }
-  } else {
-    if (achieveCardHeight[1] > achieveCardHeight[2]) {
-      setHeight = achieveCardHeight[1];
-    } else {
-      setHeight = achieveCardHeight[2];
-    }
-  }
 
-  for (let index = 0; index < achieveCard.length; index++) {
-    achieveCard[index].style.height = setHeight;
+    let setHeight;
+    if (achieveCardHeight[0] > achieveCardHeight[1]) {
+      if (achieveCardHeight[0] > achieveCardHeight[2]) {
+        setHeight = achieveCardHeight[0];
+      } else {
+        setHeight = achieveCardHeight[2];
+      }
+    } else {
+      if (achieveCardHeight[1] > achieveCardHeight[2]) {
+        setHeight = achieveCardHeight[1];
+      } else {
+        setHeight = achieveCardHeight[2];
+      }
+    }
+
+    for (let index = 0; index < achieveCard.length; index++) {
+      achieveCard[index].style.height = setHeight + "px";
+    }
   }
 }
 
 setAchieveHeight();
 window.onresize = setAchieveHeight;
-
